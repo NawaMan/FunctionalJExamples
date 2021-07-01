d600344b6263f2274d9f0d8113fcc97424f0250f
NawaMan <nawa@nawaman.net>
2021-07-01 08:28:17 -0400

    Move tests to test folder.

--- src/test/java/example/functionalj/effect/EffectExamples.java
+++ src/test/java/example/functionalj/effect/EffectExamples.java
@@ -1,11 +1,8 @@
 package example.functionalj.effect;
 
 import static functionalj.function.Func.f;
-import static functionalj.functions.RegExMatchResult.theResults;
-import static functionalj.functions.StrFuncs.matches;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -17,8 +14,6 @@ import functionalj.list.FuncList;
 import functionalj.list.ImmutableFuncList;
 import functionalj.promise.DeferAction;
 import functionalj.store.Store;
-import functionalj.stream.StreamPlus;
-import functionalj.task.Task;
 
 
 class EffectExamples {
@@ -53,37 +48,37 @@ class EffectExamples {
         assertEquals("[Result:{ Exception: java.nio.file.NoSuchFileException: fileNotFound.txt }]", logs.toString());
     }
     
-    @Test
-    public void basicTask() throws IOException {
-        // Define task
-        var wordCountOf = f((String fileName) -> 
-                Task.from(()->Files.readAllBytes(Paths.get(fileName)))
-                .map(String::new)
-                .map(matches("[a-zA-Z]+"))
-                .map(theResults.texts)
-                .map(StreamPlus::size)
-        );
-        // Define operations -> Notice that this is a generic operation -- no mention of Task.
-        var compareWordCount = f((Integer count1, Integer count2) -> {
-            return (count1 == count2) ? "Same size."
-                 : (count1 >  count2) ? "First file is larger."
-                                      : "Second file is larger.";
-        });
-        
-        // Declare tasks
-        var task1 = wordCountOf.apply("../LICENSE");
-        var task2 = wordCountOf.apply("../.travis.yml");
-        // Compose the tasks
-        var compareTask = compareWordCount.applyTo(task1, task2);
-        
-        // At this point, nothing is run.
-        
-        // Actually run
-        assertEquals("First file is larger.", compareTask.createAction().getResult().get());
-    }
+//    @Test
+//    void basicTask() throws IOException {
+//        // Define task
+//        var wordCountOf = f((String fileName) -> 
+//                Task.from(()->Files.readAllBytes(Paths.get(fileName)))
+//                .map(String::new)
+//                .map(matches("[a-zA-Z]+"))
+//                .map(theResults.texts)
+//                .map(StreamPlus::size)
+//        );
+//        // Define operations -> Notice that this is a generic operation -- no mention of Task.
+//        var compareWordCount = f((Integer count1, Integer count2) -> {
+//            return (count1 == count2) ? "Same size."
+//                 : (count1 >  count2) ? "First file is larger."
+//                                      : "Second file is larger.";
+//        });
+//        
+//        // Declare tasks
+//        var task1 = wordCountOf.apply("../LICENSE");
+//        var task2 = wordCountOf.apply("../.github/workflows/maven.yml");
+//        // Compose the tasks
+//        var compareTask = compareWordCount.applyTo(task1, task2);
+//        
+//        // At this point, nothing is run.
+//        
+//        // Actually run
+//        assertEquals("First file is larger.", compareTask.createAction().getResult().get());
+//    }
     
     @Test
-    public void storeExample() {
+    void storeExample() {
         var apppend = f((String str, ImmutableFuncList<String> list)-> list.append(str).toImmutableList());
         
         var list = FuncList.of("One", "Two");
